a3295d2334ccd321f1bb1c2233b4d60008c8ada9
Ivan Crnogorac <ivan.crnogorac@rubikscode.net>
2025-09-25 19:59:31 +0200

    Delete article moved to component

--- src/features/news/components/DeleteArticle/DeleteArticle.styles.ts
+++ src/features/news/components/DeleteArticle/DeleteArticle.styles.ts
@@ -11,5 +11,32 @@ export const createStyles = () => {
       borderRadius: 4,
       padding: 2,
     },
+    modalContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'white',
+      padding: 20,
+      height: 200, // adjust as needed
+      width: 300, // adjust as needed
+      margin: 20,
+      alignSelf: 'center', // Centers the modal itself
+      borderRadius: 10,
+      gap: 16,
+    },
+    titleText: {
+      fontWeight: 'bold',
+      fontSize: 20,
+    },
+    contentText: {
+      textAlign: 'center',
+      lineHeight: 22,
+    },
+    contentTextBold: {
+      fontWeight: 'bold',
+    },
+    buttonContainer: {
+      flexDirection: 'row',
+      gap: 8,
+    },
   });
 };
--- src/features/news/components/DeleteArticle/DeleteArticle.tsx
+++ src/features/news/components/DeleteArticle/DeleteArticle.tsx
@@ -1,25 +1,61 @@
-import { Pressable } from 'react-native';
+import { Pressable, Text, View } from 'react-native';
 import { Icon } from 'react-native-paper/src';
 import React from 'react';
 import { INewsArticle } from '../../../../types.ts';
 import { useNewsContext } from '../../context/NewsContextProvider.tsx';
 import { createStyles } from './DeleteArticle.styles.ts';
+import { Button, Modal, Portal } from 'react-native-paper';
+import { Toast } from 'toastify-react-native';
 
 interface IProps {
   article: INewsArticle;
 }
 
 export const DeleteArticle = ({ article }: IProps) => {
+  const [isModalVisible, setIsModalVisible] = React.useState(false);
   const { handleDeleteArticle } = useNewsContext();
   const styles = createStyles();
 
+  const handleDelete = () => {
+    handleDeleteArticle(article.uuid);
+    setIsModalVisible(false);
+    Toast.success('Article has been removed.');
+  };
+
   const renderDeleteArticleButton = () => (
-    <Pressable
-      onPress={() => handleDeleteArticle(article.uuid)}
-      style={styles.trigger}
-    >
+    <Pressable onPress={() => setIsModalVisible(true)} style={styles.trigger}>
       <Icon source={'trash-can-outline'} size={24} color={'white'} />
     </Pressable>
   );
-  return renderDeleteArticleButton();
+
+  const renderModal = () => (
+    <Portal>
+      <Modal
+        visible={isModalVisible}
+        onDismiss={() => setIsModalVisible(false)}
+        contentContainerStyle={styles.modalContainer}
+      >
+        <Text style={styles.titleText}>Delete Article</Text>
+        <Text style={styles.contentText}>
+          Are you sure you want to remove the article titled{' '}
+          <Text style={styles.contentTextBold}>"{article.title}"</Text>?
+        </Text>
+        <View style={styles.buttonContainer}>
+          <Button mode={'contained'} textColor={'white'} onPress={handleDelete}>
+            YES
+          </Button>
+          <Button mode={'outlined'} onPress={() => setIsModalVisible(false)}>
+            NO
+          </Button>
+        </View>
+      </Modal>
+    </Portal>
+  );
+
+  return (
+    <>
+      {renderDeleteArticleButton()}
+      {renderModal()}
+    </>
+  );
 };
