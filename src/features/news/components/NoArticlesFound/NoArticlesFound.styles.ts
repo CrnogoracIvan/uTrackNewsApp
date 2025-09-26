d79cacac9e2048453937961933c4d95152113624
Ivan Crnogorac <ivan.crnogorac@rubikscode.net>
2025-09-26 17:45:34 +0200

    NoArticlesFound.styles.ts

---
+++ src/features/news/components/NoArticlesFound/NoArticlesFound.styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+export const createStyles = () => {
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      height: '100%',
+      justifyContent: 'center',
+      alignItems: 'center',
+      gap: 12,
+    },
+  });
+};
--- src/features/news/components/NoArticlesFound/NoArticlesFound.tsx
+++ src/features/news/components/NoArticlesFound/NoArticlesFound.tsx
@@ -1,17 +1,13 @@
 import { Text, View } from 'react-native';
 import { Icon } from 'react-native-paper';
+import { createStyles } from './NoArticlesFound.styles.ts';
 
-export const NoArticlesFound = () => (
-  <View
-    style={{
-      flex: 1,
-      height: '100%',
-      justifyContent: 'center',
-      alignItems: 'center',
-      gap: 12,
-    }}
-  >
-    <Icon size={64} source={'newspaper-remove'} />
-    <Text>No articles found</Text>
-  </View>
-);
+export const NoArticlesFound = () => {
+  const styles = createStyles();
+  return (
+    <View style={styles.container}>
+      <Icon size={64} source={'newspaper-remove'} />
+      <Text>No articles found</Text>
+    </View>
+  );
+};
